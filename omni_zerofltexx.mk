2b9ca9d29ba1d74c86431b8a7b77dc304ed32012
Alvin Francis <nivlafx@gmail.com>
2016-10-05 22:45:52 -0400

    omni
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>

--- omni_zerofltexx.mk
+++ omni_zerofltexx.mk
@@ -4,15 +4,15 @@ $(call inherit-product, device/samsung/zerofltexx/full_zerofltexx.mk)
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+# Enhanced APN
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common OMNI stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="Galaxy S6" \
     TARGET_DEVICE="Galaxy S6"
 
-PRODUCT_NAME := cm_zerofltexx
+PRODUCT_NAME := omni_zerofltexx
 PRODUCT_DEVICE := zerofltexx
